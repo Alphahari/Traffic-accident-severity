941c0cac1cfb16114223f2da5fbd514033e537ac
Alphahari <harireddy1408@gmail.com>
2025-03-23 12:24:16 +0530

    Made changes to HomePage

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -3,9 +3,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <header className="flex justify-between items-center p-2 m-4">
-        <div className='flex flex-row items-center'>
-            <Link to={'/'} className='font-bold text-xl hover:text-2xl'>RAP</Link >
-        </div> 
+            <Link to={'/'} className='flex justify-center items-center font-bold text-xl hover:text-2xl'><img src="logo.png" width="40px" height="40px"></img>RAP</Link >
         <div className="flex justify-evenly items-center p-2 gap-4">
             <Link to={'/visualizations'} className="hover:font-semibold">Visualizations</Link >
             <Link to={'/features'} className="hover:font-semibold">Features</Link >
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,18 +1,46 @@
-import Navbar from '../components/Navbar'
-import Form from '../components/Form'
-import Footer from '../components/Footer'
+import Navbar from '../components/Navbar';
+import Form from '../components/Form';
+import Footer from '../components/Footer';
 
 const HomePage = () => {
   return (
     <>
-      <Navbar></Navbar>
-      <hr></hr>
-      <div className='m-2'>
-        <Form></Form>
-      </div>
-      <Footer></Footer>
-    </>
-  )
-}
+      <Navbar />
+      <hr />
+      <main className="flex flex-col flex-1 mb-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center">
+          <div className="flex flex-col md:px-32 mt-12 px-12">
+            <p className="font-bold">
+              Understand How Road Conditions Affect Accident Severity
+            </p>
+            <p className="font-medium text-gray-600 lg:text-md md:text-sm mt-2">
+              This application helps us understand how road conditions play a crucial role in accidents. By exploring various 
+              factors such as road type, weather conditions, city, pedestrian crossing facilities, junction types, and more, 
+              users can gain insights into how each element affects the severity of accidents. This interactive tool leverages 
+              robust machine learning models like XGBoost, Random Forest, and More to provide a deeper understanding of the 
+              complex relationship between road conditions and accident outcomes. With these advanced techniques, users can 
+              explore and analyze accident data to uncover patterns and improve safety measures on the roads.
+            </p>
+          </div>
+          <img className="mx-auto" src="logo.png" alt="Logo" />
+        </div>
 
-export default HomePage
+        <div className="min-h-dvh md:mt-8 px-12 md:px-32">
+          <section className="flex justify-center items-center flex-col md:p-0 sm:px-12">
+            <p className="font-bold">Predict Accident Severity with Machine Learning</p>
+            <p className="text-lg lg:w-1/2 md:w-2/3 text-gray-500 mt-2 font-semibold">
+              Our advanced machine learning models analyze road condition data to accurately predict the severity of accidents.
+              Take control of road safety with our powerful tool.
+            </p>
+          </section>
+          <div>
+            <Form />
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
